397fa958ce56a6f64f22461642b630955de8f7cf
lyq0916 <3054669628@qq.com>
2022-04-02 20:33:05 +0800

    最原始

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import {createApp} from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import installElementPlus from './plugins/element'
+import './assets/css/icon.css'
+import axios from 'axios'
+
+//设置反向代理，前端请求默认发送到8443
+axios.defaults.baseURL="http://localhost:8443/api"
+
+const app = createApp(App)
+app.config.globalProperties.$h=axios
+
+installElementPlus(app)
+app
+    .use(store)
+    .use(router)
+    .mount('#app')
---
+++ vite.config.js
@@ -0,0 +1,52 @@
+import vue from '@vitejs/plugin-vue'
+/**
+ *　　┏┓　　　┏┓+ +
+ *　┏┛┻━━━┛┻┓ + +
+ *　┃　　　　　　　┃ 　
+ *　┃　　　━　　　┃ ++ + + +
+ * ████━████ ┃+
+ *　┃　　　　　　　┃ +
+ *　┃　　　┻　　　┃
+ *　┃　　　　　　　┃ + +
+ *　┗━┓　　　┏━┛
+ *　　　┃　　　┃　　　　　　　　　　　
+ *　　　┃　　　┃ + + + +
+ *　　　┃　　　┃
+ *　　　┃　　　┃ +  神兽保佑
+ *　　　┃　　　┃    代码无bug　　
+ *　　　┃　　　┃　　+　　　　　　　　　
+ *　　　┃　 　　┗━━━┓ + +
+ *　　　┃ 　　　　　　　┣┓
+ *　　　┃ 　　　　　　　┏┛
+ *　　　┗┓┓┏━┳┓┏┛ + + + +
+ *　　　　┃┫┫　┃┫┫
+ *　　　　┗┻┛　┗┻┛+ + + +
+ */
+/*
+* vite默认端口号3000
+*
+* */
+export default {
+   server: {
+       port: 8080,
+       /* 自动打开浏览器 */
+       open: true,
+       /* 使用代理 */
+       proxy: {
+           '/api': {
+               /* 目标代理服务器地址 */
+               target: 'http://localhost:8443',
+               /* 允许跨域 */
+               changeOrigin: true,
+               pathRewrite: {
+                   "^/api": ""
+               }
+           },
+       },
+   },
+    base: './',
+    plugins: [vue()],
+    optimizeDeps: {
+        include: ['schart.js']
+    }
+}
